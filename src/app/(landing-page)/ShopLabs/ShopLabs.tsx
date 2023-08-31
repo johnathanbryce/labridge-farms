69d3b2279c2ee071faa8bfbd6f228387bd26e98d
johnathanbryce <johnathanbryce@rompathlete.com>
2023-08-31 08:14:00 -0700

    added modal for labs and fixed images sizings a bit

--- src/app/(landing-page)/ShopLabs/ShopLabs.tsx
+++ src/app/(landing-page)/ShopLabs/ShopLabs.tsx
@@ -1,16 +1,51 @@
-import React from 'react'
+'use client'
+import React, {useState} from 'react'
 import styles from './ShopLabs.module.css'
-// Next
-import Image from 'next/image'
 // Internal Components
 import LayoutContainerSections from '@/layouts/LayoutContainerSections/LayoutContainerSections'
 import Button from '@/components/Button/Button'
+import ProductModal from '@/components/ProductModal/ProductModal'
+import ProductItem from '@/components/ProductItem/ProductItem'
 // Internal Assets
 import Lily2 from '../../../../public/assets/Lily2.jpg'
 import Olive from '../../../../public/assets/Olive.jpg';
 import River2 from '../../../../public/assets/River2.jpg';
 
+const labProducts = [
+    {
+        image: Lily2,
+        name: 'Lily',
+        price: '$$$',
+        description: 'this is a more detailed description of Lily. Am ipsum dolor sit amet, consec- tetur adipiscing elit, sed do eius- mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud '
+    },
+    {
+        image: Olive,
+        name: 'Olive',
+        price: '$$$',
+        description: 'this is a more detailed description of Olive. Am ipsum dolor sit amet, consec- tetur adipiscing elit, sed do eius- mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud '
+    },
+    {
+        image: River2,
+        name: 'River',
+        price: '$$$',
+        description: 'this is a more detailed description of River. Am ipsum dolor sit amet, consec- tetur adipiscing elit, sed do eius- mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud '
+    }
+]
+
 export default function ShopLabs() {
+    const [selectedProduct, setSelectedProduct] = useState<any>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const openModal = (product: any) => {
+      setIsModalOpen(true);
+      setSelectedProduct(product);
+    };
+  
+    const closeModal = () => {
+      setIsModalOpen(false);
+      setSelectedProduct(null)
+    };
+
   return (
     <LayoutContainerSections>
         <section className={styles.shop_labs} id="farm">
@@ -20,24 +55,28 @@ export default function ShopLabs() {
         </div>
 
         <div className={styles.product_container}>
-            <div className={styles.product_wrapper}>
-                <Image src={Lily2} className={styles.product_img} alt="A picture of one of Labridge Farm's labrador retrievers"  />
-                <figcaption> <span className={styles.product_title}> Lily </span> (brief description if necessary)   </figcaption>
-            </div>
-
-            <div className={styles.product_wrapper}>
-                <Image src={Olive} className={styles.product_img} alt="A picture of one of Labridge Farm's labrador retrievers"  />
-                <figcaption> <span className={styles.product_title}> Olive </span> (brief description if necessary)   </figcaption>
-            </div>
-
-            <div className={styles.product_wrapper}>
-                <Image src={River2} className={styles.product_img} alt="A picture of one of Labridge Farm's labrador retrievers"  />
-                <figcaption> <span className={styles.product_title}> River </span> (brief description if necessary)   </figcaption>
-            </div>
+            {labProducts.map((product, index) => (
+                <ProductItem
+                    key={index}
+                    product={product}
+                    openModal={() => openModal(product)} // selects the product to trigger pop-up modal for that product
+                    isDog={true}
+                />
+            ))}
         </div>
 
         <p className={styles.disclaimer}> <span className={styles.bold}>Labridge Kennels promise:</span> a brief disclaimer or blurb about our dogs here...</p>
         </section>
+
+        {/* renders in product pop-up modals, and displays the correct one when openModal(product) is triggered */}
+        {selectedProduct && (
+            <ProductModal
+                name={selectedProduct.name}
+                description={selectedProduct.description}
+                image={selectedProduct.image}
+                onClose={closeModal}
+            />
+        )}
     </LayoutContainerSections>
   )
 }
--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -25,7 +25,7 @@ export default function ProductItem({openModal, product, isMeat, isDog}: Product
         <>
           <img src={image.src} className={styles.product_img} alt="Labridge Farm's meat"  />
           <figcaption> <span className={styles.product_title}> {name} </span>   </figcaption>
-          <p>{description} </p>
+          {/* <p>{description} </p> */}
           <p> <span className={styles.product_price}> {price} </span> contact us for more details </p>
         </>
       )}
